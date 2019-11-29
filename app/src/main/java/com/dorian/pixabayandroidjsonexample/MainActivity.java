e2a159a5b2c291ed204ba2c90028aae7a1488a8a
dorian <dev.dorian0408@gmail.com>
2019-11-29 09:45:30 +0900

    [UPDATE] 마침내 연동 1. JSON Response 생성 2. POJO 기반 데이터 넣음 3. ArrayList를 Adapter로 전달하고 RecycleerView에 Adapter를 설정해서 항목 표시

--- app/src/main/java/com/dorian/pixabayandroidjsonexample/MainActivity.java
+++ app/src/main/java/com/dorian/pixabayandroidjsonexample/MainActivity.java
@@ -1,14 +1,83 @@
 package com.dorian.pixabayandroidjsonexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private ExampleAdapter mExampleAdapter;
+    private ArrayList<ExampleItem> mExampleItemArrayList;
+    private RequestQueue mRequestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mExampleItemArrayList = new ArrayList<>();
+
+        mRequestQueue = Volley.newRequestQueue(this);
+        parseJSON();
+    }
+
+    private void parseJSON() {
+//        String url = "https://pixabay.com/api/?key=14444837-7282363c726bab83673484748&q=yellow+flowers&image_type=photo&pretty=true";
+        String url = "https://pixabay.com/api/?key=14444837-7282363c726bab83673484748&q=earth&image_type=photo&pretty=true";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("hits");
+                            for (int i=0;i<jsonArray.length();i++){
+                                JSONObject hit = jsonArray.getJSONObject(i);
+
+                                String creatorName = hit.getString("user");
+                                String imageUrl = hit.getString("webformatURL");
+                                int likeCount = hit.getInt("likes");
+
+                                mExampleItemArrayList.add(new ExampleItem(imageUrl,creatorName,likeCount));
+                            }
+
+                            mExampleAdapter = new ExampleAdapter(MainActivity.this,mExampleItemArrayList);
+                            mRecyclerView.setAdapter(mExampleAdapter);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mRequestQueue.add(request);
+
     }
 }
